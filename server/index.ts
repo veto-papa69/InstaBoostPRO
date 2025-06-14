b8f26cc1bf92e0140387d243860ea3933b7336e0
veto-papa69 <keturahbidd87@gmail.com>
2025-06-14 19:37:59 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { APP_CONFIG } from "./config.js";
 import { registerRoutes } from "./routes.js";
-import { MongoDBStorage } from "./mongodb-storage.js";
+import { MongoDBStorage } from "./mongodb-storage";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
